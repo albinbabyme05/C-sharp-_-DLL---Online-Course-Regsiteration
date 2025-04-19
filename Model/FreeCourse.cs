670a1db81c35a3b0bf79392e4f6f8e1b191a6e5b
Albin Baby <albinbabayme05@gmail.com>
2025-04-19 11:19:36 +0200

    free course class create- regsitered user can access only free course

--- Model/FreeCourse.cs
+++ Model/FreeCourse.cs
@@ -6,16 +6,12 @@ using System.Threading.Tasks;
 
 namespace Online_Course_Registration_System.Model
 {
-    internal class FreeCourse : CourseLibrary
+    public class FreeCourse : CourseLibrary
     {
         public FreeCourse(string title, string instructor) : base(title, instructor)
         {
         }
 
-        public override bool Enroll(User user)
-        {
-            Console.WriteLine("Please Register to Enroll for the Course !!!");
-            return false;
-        }
+        
     }
 }
