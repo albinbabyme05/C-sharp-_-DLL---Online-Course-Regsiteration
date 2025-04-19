05b45547912b865253453b1d97c8363f5e55e9d6
Albin Baby <albinbabayme05@gmail.com>
2025-04-19 16:03:22 +0200

    registration for paid course done

--- User/RegisteredUser.cs
+++ User/RegisteredUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Online_Course_Registration_System.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,35 @@ using System.Threading.Tasks;
 
 namespace Online_Course_Registration_System.User
 {
-    internal class RegisteredUser
+    public class RegisteredUser : User
     {
+        private double WalletBalance;
+        private Guid UserId { get; set; }
+        public RegisteredUser(string name, string email) : base(name, email)
+        {
+            WalletBalance = 0;
+            UserId = Guid.NewGuid();
+        }
+
+        public double GetWalletBalance() => WalletBalance;
+        public void SetWalletBalance(double walletbalance) { WalletBalance = walletbalance; }
+
+        public override string RegsiterCourse(CourseLibrary course)
+        {
+            if (course is FreeCourse)
+            {
+                return $"You are enrolled for {course.GetTitle()} for free";
+            }
+            else if (course is PaidCourse pc)
+            {
+                if(WalletBalance >= pc.GetPrice())
+                {
+                    WalletBalance -= pc.GetPrice();
+                    return $"You are enrolled for {course.GetTitle()} for {pc.GetPrice()}";
+                }
+                return $"inSuffiecent balance to enroll for {course.GetTitle()}";
+            }
+            return $" Something went wrong...Try again.";
+        }
     }
 }
