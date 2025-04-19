65c70a428d125195e1603ac6b6d21233b4259f4c
Albin Baby <albinbabayme05@gmail.com>
2025-04-19 11:36:40 +0200

    aaded method to add course, get paid and free course

--- Service/CourseManager.cs
+++ Service/CourseManager.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Online_Course_Registration_System.Model;
 
 namespace Online_Course_Registration_System.Service
 {
-    internal class CourseManager
+    public class CourseManager
     {
+        private List<CourseLibrary> courseList = new List<CourseLibrary>();
+
+        public void AddCourse(CourseLibrary course)
+        {
+            courseList.Add(course);
+        }
+
+        public List<PaidCourse> GetPaidCourses()
+        {
+            List<PaidCourse> paidCourseList = new List<PaidCourse>();
+            foreach (var course in courseList)
+            {
+                if (course is PaidCourse pc)
+                {
+                    paidCourseList.Add(pc);
+                }
+            }
+            return paidCourseList;
+        }
+
+        public List<FreeCourse> GetFreeCourses()
+        {
+            List<FreeCourse> freeCourseList = new List<FreeCourse>();
+            foreach (var course in courseList)
+            {
+                if (course is FreeCourse pc)
+                {
+                    freeCourseList.Add(pc);
+                }
+            }
+            return freeCourseList;
+        }
     }
 }
